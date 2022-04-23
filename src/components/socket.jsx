215b946e21b89d9752cf0d3a7c155c3fe302d87c
‘DmitryK1995’ <KolesnikovD-S@yandex.ru>
2022-04-23 16:27:54 +0300

    socket fix

--- src/components/socket.jsx
+++ src/components/socket.jsx
@@ -3,7 +3,7 @@ import store from '../slices/index.js';
 import { actions as messagesActions } from '../slices/messagesSlice.js';
 import { actions as channelsActions } from '../slices/channelsSlice.js';
 
-const socket = io('ws://localhost:5000');
+const socket = io();
 
 const sendMessage = (message, username, channelId) => socket.emit('newMessage', { message, username, channelId });
 
